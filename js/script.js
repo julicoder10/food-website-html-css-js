20a6b275f571a630f01d9c2073316e934e10ce13
Julicoder10 <julian.andres.aquino33@gmail.com>
2023-04-29 14:02:14 -0300

    cambie algunas demos

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
-let searchForm = document.querySelector('.search-form-container');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    cart.classList.remove('active');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
-}
-
-
 let cart = document.querySelector('.shopping-cart-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
     cart.classList.toggle('active');
-    searchForm.classList.remove('active');
     navbar.classList.remove('active');
     loginForm.classList.remove('active');
 }
@@ -22,7 +11,6 @@ let loginForm = document.querySelector('.login-form-container');
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
     cart.classList.remove('active');
-    searchForm.classList.remove('active');
     navbar.classList.remove('active');
 }
 
@@ -33,7 +21,6 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     loginForm.classList.remove('active');
     cart.classList.remove('active');
-    searchForm.classList.remove('active');
     
 }
 
@@ -47,5 +34,5 @@ document.querySelector('.home').onmousemove = (e) =>{
     let x = (window.innerWidth - e.pageX * 2) / 90;
     let y = (window.innerHeight - e.pageY * 2) / 90;
 
-    document.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px)translateY(${x}px)`;
+    document.querySelector('.parallax-img').style.transform = `translateX(${y}px)translateY(${x}px)`;
 }
